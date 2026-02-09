f0786c11d447c4c75e7dd2644048b2a8b97c24cc
ambikamanchem27 <ambikamanchem27@gmail.com>
2026-02-09 11:33:42 +0530

    Final autograder-safe ROS2 publisher node

--- src/publisher_node.cpp
+++ src/publisher_node.cpp
@@ -1,5 +1,7 @@
 #include <chrono>
 #include <memory>
+#include <string>
+
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
@@ -9,11 +11,13 @@ class PublisherNode : public rclcpp::Node
 {
 public:
   PublisherNode()
-  : Node("publisher_node")
+  : Node("publisher_node"), count_(0)
   {
     publisher_ = this->create_publisher<std_msgs::msg::String>("/chatter", 10);
+
     timer_ = this->create_wall_timer(
-      500ms, std::bind(&PublisherNode::timer_callback, this)
+      500ms,
+      std::bind(&PublisherNode::timer_callback, this)
     );
   }
 
@@ -21,13 +25,17 @@ private:
   void timer_callback()
   {
     auto message = std_msgs::msg::String();
-    message.data = "Hello ROS2";
+    message.data = "Count: " + std::to_string(count_);
+
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+
     publisher_->publish(message);
+    count_++;
   }
 
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   rclcpp::TimerBase::SharedPtr timer_;
+  int count_;
 };
 
 int main(int argc, char * argv[])
@@ -37,4 +45,3 @@ int main(int argc, char * argv[])
   rclcpp::shutdown();
   return 0;
 }
-
